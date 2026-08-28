62803b106b26234bfde79c9d15953e693898fc4a
ฺB.CJ <support@b-tctraining.com>
2026-08-28 14:37:55 +0700

    feat: add SiteCost service worker

---
+++ public/site-cost-wa/sw.js
@@ -0,0 +1,1 @@
+const C='sitecost446-v3',A=['./','./index.html','./manifest.webmanifest','./data.js','./app.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
